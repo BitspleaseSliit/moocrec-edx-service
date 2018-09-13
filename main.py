91557e3ebf7fc72a0df276af7edb59260e806499
sajeeva <sa.hasith@gmail.com>
2018-09-14 01:00:23 +0530

    de;lete method implement - not test

--- main.py
+++ main.py
@@ -7,16 +7,10 @@ from db.edx_coursedb import ( EdxCourses )
 def deleteDownloadContent(course):
     print("deleting files ....... ")
 
-    jsonFile = course["path"] + "-syllabus-parsed.json"
+    downloadPath = "Downloaded" + course["path"]
 
-    if os.path.exists(jsonFile):
-        os.remove(jsonFile)
-    else:
-        print("The file does not exist")
-        return False
-
-    if os.path.exists(course["path"]):
-        shutil.rmtree(course["path"])
+    if os.path.exists(downloadPath):
+        shutil.rmtree(downloadPath)
     else:
         print("The file does not exist")
         return False
@@ -27,7 +21,7 @@ if __name__ == '__main__':
     courses = EdxCourses()
     print(courses.getDownloadCoursesPaths())
     download_courses = courses.getDownlodCourses()
-
+    # scrapper("https://courses.edx.org/courses/course-v1:UCSanDiegoX+DSE200x+3T2018/course/")
     for course in download_courses:
         if scrapper(course["courseUrl"]):
             courses.updateProcessedTrue(course)
